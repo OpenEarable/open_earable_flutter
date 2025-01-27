a895cad8c94cd3b5a891b0df407052ea1decf9a2
Jonas Greifenhain <cadivus@daverkomp.de>
2025-01-27 14:41:16 +0100

    Add wearable function for getting device icon

--- lib/src/models/devices/wearable.dart
+++ lib/src/models/devices/wearable.dart
@@ -12,6 +12,19 @@ abstract class Wearable {
     disconnectNotifier.addListener(_notifyDisconnectListeners);
   }
 
+  /// Gets path to an icon representing the wearable.
+  /// Preferred type is SVG.
+  /// Needs to be added to the asset section of the pubspec.yaml file.
+  /// When setting the path here, keep in mind it's a lib
+  /// ('packages/open_earable_flutter/assets/...').
+  ///
+  /// The parameters are best-effort
+  ///
+  /// @param darkmode: Whether the icon should be for dark mode (if available).
+  String? getWearableIconPath({bool darkmode = false}) {
+    return null;
+  }
+
   final List<VoidCallback> _disconnectListeners = [];
 
   void addDisconnectListener(VoidCallback listener) {
