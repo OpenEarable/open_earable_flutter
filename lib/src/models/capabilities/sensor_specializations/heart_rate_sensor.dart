dcc2020e1f40700079ca505b6c7605b59be4fb24
Jonas Greifenhain <cadivus@daverkomp.de>
2025-02-27 12:27:02 +0100

    Specialize heart rate sensor for Cosinuss One and Polar

---
+++ lib/src/models/capabilities/sensor_specializations/heart_rate_sensor.dart
@@ -0,0 +1,35 @@
+import '../sensor.dart';
+
+abstract class HeartRateSensor extends Sensor {
+  const HeartRateSensor({
+    super.relatedConfigurations = const [],
+  }) : super(
+          sensorName: 'HR',
+          chartTitle: 'Heart Rate',
+          shortChartTitle: 'HR',
+        );
+
+  @override
+  List<String> get axisNames => ['Heart Rate'];
+
+  @override
+  List<String> get axisUnits => ['BPM'];
+
+  @override
+  int get axisCount => 1;
+
+  @override
+  Stream<HeartRateSensorValue> get sensorStream;
+}
+
+class HeartRateSensorValue extends SensorIntValue {
+  HeartRateSensorValue({
+    required int heartRateBpm,
+    required int timestamp,
+  }) : super(
+          values: [heartRateBpm],
+          timestamp: timestamp,
+        );
+
+  int get heartRateBpm => values.first;
+}
