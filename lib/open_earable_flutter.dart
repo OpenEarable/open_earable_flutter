2a025d61a5999f0e03cab9f0b0f760dca1e25c7a
Jonas Greifenhain <cadivus@daverkomp.de>
2025-04-05 18:27:29 +0200

    TMP disable logs

--- lib/open_earable_flutter.dart
+++ lib/open_earable_flutter.dart
@@ -40,7 +40,7 @@ export 'src/models/capabilities/storage_path_audio_player.dart';
 export 'src/models/capabilities/sensor_config_capabilities/recordable_sensor_config.dart';
 export 'src/models/capabilities/sensor_config_capabilities/streamable_sensor_configuration.dart';
 
-Logger logger = Logger();
+Logger logger = Logger(level: Level.off);
 
 class WearableManager {
   static final WearableManager _instance = WearableManager._internal();
