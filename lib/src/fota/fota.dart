44671deeabb8ac38340c27e326f57b6ef6b57e06
Oliver Bagge <bagge@teco.edu>
2025-05-21 14:52:14 +0200

    remove UI widgets from lib

---
+++ lib/src/fota/fota.dart
@@ -0,0 +1,5 @@
+export 'bloc/bloc/update_bloc.dart';
+export 'model/firmware_update_request.dart';
+export 'model/firmware_image.dart';
+export 'providers/firmware_update_request_provider.dart';
+export 'repository/firmware_image_repository.dart';
