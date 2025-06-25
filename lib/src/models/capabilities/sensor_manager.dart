6c5b09e9370c2eef591db3da703677a68011bf73
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-06-25 15:03:46 +0200

    lib/src/models/capabilities/sensor_manager.dart: added documentation

--- lib/src/models/capabilities/sensor_manager.dart
+++ lib/src/models/capabilities/sensor_manager.dart
@@ -1,5 +1,8 @@
 import 'sensor.dart';
 
+/// A device that manages a collection of sensors.
 abstract class SensorManager {
+  /// The sensors managed by this device.
+  /// This is a read-only list of sensors.
   List<Sensor> get sensors;
 }
