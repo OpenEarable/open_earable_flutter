7b8a455c70de122b7855de0ad2e4dd0037813ff0
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-06-27 19:40:44 +0200

    lib/src/models/capabilities/sensor_configuration_specializations/configurable_sensor_configuration.dart: added documentation

--- lib/src/models/capabilities/sensor_configuration_specializations/configurable_sensor_configuration.dart
+++ lib/src/models/capabilities/sensor_configuration_specializations/configurable_sensor_configuration.dart
@@ -2,7 +2,11 @@ import 'package:flutter/foundation.dart';
 
 import '../sensor_configuration.dart';
 
+/// Base class for sensor configuration options.
+/// This class represents a configuration option that can be applied to a [ConfigurableSensorConfiguration].
 abstract class SensorConfigurationOption {
+  /// The name of the configuration option.
+  /// This name is used to identify the option in the UI or logs.
   final String name;
 
   const SensorConfigurationOption({
@@ -25,12 +29,20 @@ abstract class SensorConfigurationOption {
   }
 }
 
+/// A [SensorConfiguration] specialization that allows for configurable options.
+/// This class extends [SensorConfiguration] and adds a set of available options that can be applied
+/// to the configuration values. It is designed to be used with [ConfigurableSensorConfigurationValue].
+/// This class provides a way to manage sensor configurations that can have multiple behaviors based on the options applied.
 abstract class ConfigurableSensorConfiguration<SCV extends ConfigurableSensorConfigurationValue> extends SensorConfiguration<SCV> {
+  /// A set of all available options for values of this configuration.
   final Set<SensorConfigurationOption> availableOptions;
 
   ConfigurableSensorConfiguration({required super.name, required super.values, this.availableOptions = const {}});
 }
 
+/// A base class for sensor configuration values that can be configured with options.
+/// This class extends [SensorConfigurationValue] and adds a set of options that can be applied
+/// to the configuration value. It is designed to be used with [ConfigurableSensorConfiguration].
 abstract class ConfigurableSensorConfigurationValue extends SensorConfigurationValue {
   final Set<SensorConfigurationOption> options;
 
