5978e8d53941e763141db61cdb43910133b3b294
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-07-22 14:41:57 +0200

    lib/src/models/wearable_factory.dart: use BleGattManager instead of BleManager

--- lib/src/models/wearable_factory.dart
+++ lib/src/models/wearable_factory.dart
@@ -1,11 +1,20 @@
 import 'package:open_earable_flutter/open_earable_flutter.dart';
-import 'package:open_earable_flutter/src/managers/ble_manager.dart';
 import 'package:universal_ble/universal_ble.dart';
 
+import '../managers/ble_gatt_manager.dart';
+
+/// Abstract factory for creating [Wearable] instances from [DiscoveredDevice]s.
 abstract class WearableFactory {
-  BleManager? bleManager;
+  /// The bleManager is used to perform GATT operations on the wearable.
+  /// It is provided by the [WearableManager] and should not be set directly.
+  BleGattManager? bleManager;
+
+  /// The disconnect notifier is used to notify listeners when the wearable is disconnected.
+  /// It is provided by the [WearableManager] and should not be set directly.
   WearableDisconnectNotifier? disconnectNotifier;
 
+  /// Checks if the factory can create a wearable from the given device and services.
   Future<bool> matches(DiscoveredDevice device, List<BleService> services);
+  /// Creates a wearable from the given device.
   Future<Wearable> createFromDevice(DiscoveredDevice device);
 }
