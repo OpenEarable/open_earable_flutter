b7c253b46e5e6a29bbd53d08c4aa1ed4dde5b57f
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-08-21 15:38:33 +0200

    lib/src/models/capabilities/stereo_device.dart: added StereoDevice capability

---
+++ lib/src/models/capabilities/stereo_device.dart
@@ -0,0 +1,12 @@
+enum DevicePosition {
+  left,
+  right
+}
+
+abstract class StereoDevice {
+  Future<DevicePosition> get position;
+  Future<StereoDevice?> get pairedDevice;
+
+  Future<void> pair(StereoDevice device);
+  Future<void> unpair();
+}
