3ec9ee6a3fbc5168abae141b5547b7ca1dfbb181
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2025-09-12 14:37:10 +0200

    lib/src/utils/sensor_value_parser/sensor_value_parser.dart: return list of maps for possible buffered sensor data

--- lib/src/utils/sensor_value_parser/sensor_value_parser.dart
+++ lib/src/utils/sensor_value_parser/sensor_value_parser.dart
@@ -3,5 +3,6 @@ import 'dart:typed_data';
 import '../sensor_scheme_parser/sensor_scheme_reader.dart';
 
 abstract class SensorValueParser {
-  Map<String, dynamic> parse(ByteData data, List<SensorScheme> sensorSchemes);
+  /// Parses raw sensor data bytes into a list of [Map]s of sensor values.
+  List<Map<String, dynamic>> parse(ByteData data, List<SensorScheme> sensorSchemes);
 }
