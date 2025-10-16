74528d0c4cf5b2ddf2e9d59e1081dd2253aa46fe
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2025-10-16 13:30:37 +0300

    lib/src/models/capabilities/sensor_specializations/tau_ring/tau_ring_sensor.dart: implemented tau ring sensor

---
+++ lib/src/models/capabilities/sensor_specializations/tau_ring/tau_ring_sensor.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import '../../../../managers/sensor_handler.dart';
+import '../../sensor.dart';
+
+class TauRingSensor extends Sensor<SensorIntValue> {
+  const TauRingSensor({
+    required this.sensorId,
+    required super.sensorName,
+    required super.chartTitle,
+    required super.shortChartTitle,
+    required List<String> axisNames,
+    required List<String> axisUnits,
+    required this.sensorHandler,
+    super.relatedConfigurations = const [],
+  }) : _axisNames = axisNames, _axisUnits = axisUnits;
+
+  final int sensorId;
+  final List<String> _axisNames;
+  final List<String> _axisUnits;
+
+  final SensorHandler sensorHandler;
+
+  @override
+  List<String> get axisNames => _axisNames;
+
+  @override
+  List<String> get axisUnits => _axisUnits;
+
+  @override
+  int get axisCount => _axisNames.length;
+
+  @override
+  Stream<SensorIntValue> get sensorStream {
+    StreamController<SensorIntValue> streamController = StreamController();
+    sensorHandler.subscribeToSensorData(sensorId).listen(
+      (data) {
+        int timestamp = data["timestamp"];
+
+        List<int> values = [];
+        for (var entry in (data as Map).entries) {
+          if (entry.key == 'units') {
+            continue;
+          }
+
+          values.add(entry.value.toDouble());
+        }
+
+        SensorIntValue sensorValue = SensorIntValue(
+          values: values,
+          timestamp: timestamp,
+        );
+
+        streamController.add(sensorValue);
+      },
+    );
+    return streamController.stream;
+  }
+}
