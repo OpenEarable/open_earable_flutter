aeb0ae8129338f5ba15839956498c147cbe156b5
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2025-10-31 00:24:17 +0100

    lib/src/models/capabilities/device_firmware_version.dart: use Version from semver instead of custom implementation

--- lib/src/models/capabilities/device_firmware_version.dart
+++ lib/src/models/capabilities/device_firmware_version.dart
@@ -1,8 +1,10 @@
+import 'package:pub_semver/pub_semver.dart';
+
 import '../../../open_earable_flutter.dart' show logger;
-import 'version_number.dart';
 
 enum FirmwareSupportStatus {
   supported,
+  unsupported,
   tooOld,
   tooNew,
   unknown,
@@ -10,18 +12,20 @@ enum FirmwareSupportStatus {
 
 abstract class DeviceFirmwareVersion {
   Future<String?> readDeviceFirmwareVersion();
-  Future<VersionNumber?> readFirmwareVersionNumber();
+  Future<Version?> readFirmwareVersionNumber();
+
+  VersionConstraint get supportedFirmwareRange;
 
   Future<FirmwareSupportStatus> checkFirmwareSupport();
 }
 
 mixin DeviceFirmwareVersionNumberExt implements DeviceFirmwareVersion {
   @override
-  Future<VersionNumber?> readFirmwareVersionNumber() async {
+  Future<Version?> readFirmwareVersionNumber() async {
     final ver = await readDeviceFirmwareVersion();
     if (ver == null) return null;
     try {
-      return VersionNumber.parse(ver);
+      return Version.parse(ver);
     } catch (e) {
       logger.w('Failed to parse firmware version: $ver, error: $e');
       return null;
@@ -30,6 +34,28 @@ mixin DeviceFirmwareVersionNumberExt implements DeviceFirmwareVersion {
 
   @override
   Future<FirmwareSupportStatus> checkFirmwareSupport() async {
+    final Version? ver = await readFirmwareVersionNumber();
+
+    if (ver == null) return FirmwareSupportStatus.unknown;
+
+    if (!supportedFirmwareRange.allows(ver)) {
+      if (supportedFirmwareRange is! VersionRange) {
+        return FirmwareSupportStatus.unsupported;
+      }
+
+      final range = supportedFirmwareRange as VersionRange;
+      if (range.max != null && ver > range.max!) {
+        return FirmwareSupportStatus.tooNew;
+      } else if (range.min != null && ver < range.min!) {
+        return FirmwareSupportStatus.tooOld;
+      } else {
+        return FirmwareSupportStatus.unsupported;
+      }
+    }
+
     return FirmwareSupportStatus.supported;
   }
+
+  @override
+  VersionConstraint get supportedFirmwareRange => VersionConstraint.any;
 }
