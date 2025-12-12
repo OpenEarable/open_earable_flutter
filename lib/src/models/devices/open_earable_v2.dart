5bb75a77e10f5d16170645875c20ab31251b0296
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2025-12-12 15:37:28 +0100

    lib/src/models/devices/open_earable_v2.dart: changed mean magnitude format to 5.3 for audio response

--- lib/src/models/devices/open_earable_v2.dart
+++ lib/src/models/devices/open_earable_v2.dart
@@ -581,7 +581,8 @@ class OpenEarableV2 extends BluetoothWearable
     }
 
     final int quality = data[1];
-    final int meanMagnitude = data[2];
+    final int meanMagnitudeRaw = data[2];
+    final double meanMagnitude = meanMagnitudeRaw / 8.0;
     final int numPeaks = data[3];
 
     // Frequencies: 9 * uint16_t (12.4 fixed point) starting at offset 4
@@ -618,6 +619,7 @@ class OpenEarableV2 extends BluetoothWearable
       'version': version,
       'quality': quality,
       'mean_magnitude': meanMagnitude,
+      'mean_magnitude_raw': meanMagnitudeRaw,
       'num_peaks': numPeaks,
       'frequencies_hz': frequenciesHz,
       'frequencies_raw_q12_4': frequenciesRaw,
