5cee664c623bc03e7a747123653085554b695e58
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2025-12-12 15:20:07 +0100

    lib/open_earable_flutter.dart: export AudioResponseManager

--- lib/open_earable_flutter.dart
+++ lib/open_earable_flutter.dart
@@ -68,6 +68,7 @@ export 'src/models/wearable_factory.dart';
 export 'src/models/capabilities/system_device.dart';
 export 'src/managers/ble_gatt_manager.dart';
 export 'src/models/capabilities/time_synchronizable.dart';
+export 'src/models/capabilities/audio_response_manager.dart';
 
 export 'src/fota/fota.dart';
 
