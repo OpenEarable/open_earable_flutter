57db2ada90e0780bbafd9ddb0d390f823f0451d2
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2026-01-08 14:02:13 +0100

    lib/src/models/devices/wearable.dart: added capability registry to Wearable as a new way of implementing capabilities

--- lib/src/models/devices/wearable.dart
+++ lib/src/models/devices/wearable.dart
@@ -5,11 +5,47 @@ import '../../managers/wearable_disconnect_notifier.dart';
 abstract class Wearable {
   final String name;
 
+  final List<dynamic> _capabilities = [];
+
   Wearable({
     required this.name,
     required WearableDisconnectNotifier disconnectNotifier,
   }) {
     disconnectNotifier.addListener(_notifyDisconnectListeners);
+    _capabilities.add(this);
+  }
+
+  bool hasCapability<T>() {
+    for (final capability in _capabilities) {
+      if (capability is T) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  T? getCapability<T>() {
+    for (final capability in _capabilities) {
+      if (capability is T) {
+        return capability;
+      }
+    }
+    return null;
+  }
+
+  T requireCapability<T>() {
+    final capability = getCapability<T>();
+    if (capability != null) {
+      return capability;
+    }
+    throw StateError('Wearable does not have required capability: $T');
+  }
+
+  void registerCapability<T>(T capability) {
+    if (hasCapability<T>()) {
+      throw StateError('Wearable already has capability: $T');
+    }
+    _capabilities.add(capability);
   }
 
   /// Gets path to an icon representing the wearable.
