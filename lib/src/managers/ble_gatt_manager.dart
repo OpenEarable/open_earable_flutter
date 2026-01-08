6414aaafab52af5116b519a81601fc8141f2c459
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2026-01-08 15:14:08 +0100

    lib/src/managers: added function to check for presence of specific services and characteristics of a ble device

--- lib/src/managers/ble_gatt_manager.dart
+++ lib/src/managers/ble_gatt_manager.dart
@@ -3,6 +3,18 @@ abstract class BleGattManager {
   /// Check if a device is connected.
   bool isConnected(String deviceId);
 
+  /// Checks if a specific service is available on the connected device.
+  Future<bool> hasService({
+    required String deviceId,
+    required String serviceId,
+  });
+
+  Future<bool> hasCharacteristic({
+    required String deviceId,
+    required String serviceId,
+    required String characteristicId,
+  });
+
   /// Writes byte data to a specific characteristic of a device.
   Future<void> write({
     required String deviceId,
